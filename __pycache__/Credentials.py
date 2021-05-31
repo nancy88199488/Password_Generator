e4bed6ed73d21c709aebd9c789f8b730a98377f3
nancy88199488 <nancy.moranga@student.moringaschool.com>
2021-05-31 06:34:35 +0300

    add the save method to save item ino the list

--- __pycache__/Credentials.py
+++ __pycache__/Credentials.py
@@ -2,5 +2,15 @@
      '''
       Created class for the credentials
       '''
-       def __init__(self, account_name, account_password)
-       
+      credentials_list = []
+
+     def  __init__(self, account_name,  account_password);
+     self.account_name = account_name
+     self.account_password = account_password
+
+     def save_credentials(self):
+         '''
+         Method that saves the credentials object into the credentials_list
+         '''
+         
+
