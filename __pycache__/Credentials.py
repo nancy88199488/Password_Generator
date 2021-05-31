076bf7a8223be09d668275d7b03a09a4e378cd7e
nancy88199488 <nancy.moranga@student.moringaschool.com>
2021-05-31 07:15:17 +0300

    add the display method to display the credentials

--- __pycache__/Credentials.py
+++ __pycache__/Credentials.py
@@ -20,5 +20,29 @@
              '''
              @classmethod
              def credentials_exists(cls, name):
+                 '''Method  that checks if a credential exists from the credentials list
+                 Args:
+                 name: name of the account to search if it exists
+                 boolean: ttrue or false depending if the credential exists
+                 '''
+                 for credential in cls.credentials_list:
+                     if credential.account_name == name:
+                         return True
+                         return False
+                         def find_by_name(cls, account_name):
+                             ''' 
+                             Method that finds a credential and returns the credential that matches
+                             Args: 
+                             name: account_name that has a password
+                             return:the account_name and it's corresponding password
+                             '''
+                             for credential in cls.credentials_list:
+                                 if credential.account_name ==account_name:
+                                     return credential
+                                     @classmethod
+                                     def display_credentials(cls):
+                                         '''Method that displays the current credentials
+                                         '''
+                                         return cls.credentials_list
 
 
