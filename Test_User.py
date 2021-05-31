0e95e650d550d486e68dec6c2dc511156c6b1ec1
nancy88199488 <nancy.moranga@student.moringaschool.com>
2021-05-31 06:03:02 +0300

    Correctly formatted the test user

--- Test_User.py
+++ Test_User.py
@@ -1,5 +1,5 @@
 import unittest
-from User import User, user
+from User import User
 
 class TestUser(unittest.TestCase):
         '''
@@ -13,23 +13,23 @@ def setUp(self):
         Set up method to run before each test cases  to check if the class has been instantiated correctly
 
         '''
-        self.new_user  = user("NewUser", "963258741")
+        self.new_user  = User("NewUser", "963258741")
 
-        def test_init(self):
+def test_init(self):
             '''
             Test to ensure that the object is initialized properly
             '''
             self.assertEqual(self.new_user.user_name, "NewUser")
             self.assertEqual(self.new_user.password,  "963258741")
 
-            def test_save_user(self):
+def test_save_user(self):
                 '''
                   test_save_user test case to test if the user object is saved into the user list
                 '''
                 self.new_user.save_user()
                 self.assertEqual(len(User.user_list),1)
 
-                if __name__  ==  '__main__':
+if __name__  ==  '__main__':
                     unittest.main()
 
 
