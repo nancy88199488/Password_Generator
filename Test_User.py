8c84692ed1e4b5abb70d4727a6e1e56add92d87e
nancy88199488 <nancy.moranga@student.moringaschool.com>
2021-05-31 05:37:40 +0300

    addd test init to ensure the object is initialized

--- Test_User.py
+++ Test_User.py
@@ -2,8 +2,22 @@ import unittest
 from User import user
 
 class TestUser(unittest.TestCase):
-    '''
-    Test class that defines test cases for the user class behaviours
-    '''
+        '''
+    Test class that defines test cases for the user class behaviours.
 
-    def setUp(self):
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+def setUp(self):
+        '''
+        Set up method to run before each test cases  to check if the class has been instantiated correctly
+
+        '''
+        self.new_user  = user("NewUser", "963258741")
+        def test_init(self):
+            '''
+            Test to ensure that the object is initialized properly
+            '''
+            
+            
+    
